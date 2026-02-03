4eb97ebeebdd0eed00afe27544480f9f0e23ffed
GabrieleFoti <60048589+GabrieleFoti@users.noreply.github.com>
2026-02-03 15:14:33 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '';
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://test.hostlyflorence.it',
+  origin: process.env.FRONTEND_URL || 'https://test.hostlyflorence.it',
   credentials: true
 };
 app.use(cors(corsOptions));
